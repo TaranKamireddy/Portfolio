7538ab5bf693438119d20850bf5cb14461ee535b
Meluhn <tarankamireddy@gmail.com>
2023-09-30 12:49:11 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./styles/modern-normalize.css"
 import "./styles/App.css"
-import "./styles/hero.css"
+import "./styles/components/hero.css"
 import "./styles/utils.css"
+
+import Navbar from "./Navbar"
 
 export default function App() {
   return (
-    <main className="hero container">
-      <h1>Responsive</h1>
-      <button className="btn">Blue Button</button>
-    </main>
+      <main className="hero container">
+        <Navbar />
+        <h1>Responsive</h1>
+        <button className="btn">Blue Button</button>
+      </main>
   )
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,31 @@
+// import logo from '../images/logo.svg'
+import './styles/Navbar.css'
+
+export default function Navbar() {
+  return (
+        <div className="navbar">
+          <div className="navbarcontainer">
+            <a className="navbar-text" href="/">
+              {/* <img className="logo-icon" src={logo} alt="logo" draggable="false"></img> */}
+            </a>
+            <div className="navbar-menu">
+              <div className="navbar-link">
+                <a className="navbar-text" href="/news">News</a>
+              </div>
+              <div className="navbar-link">
+                <a className="navbar-text" href="/services">Services</a>
+              </div>
+              <div className="navbar-link">
+                <a className="navbar-text" href="/resources">Resources</a>
+              </div>
+              <div className="navbar-link">
+                <a className="navbar-text" href="/about">About</a>
+              </div>
+              <div className="navbar-button">
+                <a className="navbar-button-text" href="/contact">Contact</a>
+              </div>
+            </div>
+          </div>
+        </div>
+  )
+ }
