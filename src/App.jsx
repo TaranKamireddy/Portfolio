2e6eaba38f51da8f496baa21cd41e718d81e61c1
Brian Tran <briantra23@gmail.com>
2023-09-30 13:08:34 -0400

    wwww

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./styles/modern-normalize.css"
 import "./styles/App.css"
 import "./styles/hero.css"
 import "./styles/utils.css"
+import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
     <main className="hero container">
+      <Spline scene="https://prod.spline.design/8Y03I9FCMw6NfOjT/scene.splinecode" />
       <h1>Responsive</h1>
       <button className="btn">Blue Button</button>
     </main>
